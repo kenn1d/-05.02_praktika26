07db7a8115ba9ad2516d885da88a66cb7c10f1b6
kenn1d <makarkibanov220@gmail.com>
2026-03-14 02:26:13 +0500

    Процесс реализации сортировки

--- Pages/Clubs/Main.xaml.cs
+++ Pages/Clubs/Main.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.EntityFrameworkCore;
 using praktika26.Classes;
 
 namespace praktika26.Pages.Clubs
@@ -18,5 +20,35 @@ namespace praktika26.Pages.Clubs
 
         private void AddClub(object sender, System.Windows.RoutedEventArgs e) =>
             MainWindow.init.OpenPages(new Pages.Clubs.Add(this));
+
+        private void SortName(object sender, System.Windows.RoutedEventArgs e) =>
+            Search("Name");
+
+        private void SortAddress(object sender, RoutedEventArgs e) =>
+            Search("Address");
+
+        private void SortTime(object sender, RoutedEventArgs e) =>
+            Search("Time");
+
+        public void Search(string type)
+        {
+            if (SearchParam.Text != "")
+            {
+                IEnumerable<Models.Clubs> SearchClubs = AllClub.Clubs;
+                IEnumerable<Models.Clubs> Result = SearchClubs;
+                if (type == "Name") Result = SearchClubs.Where(x => x.Name.ToLower().Contains(SearchParam.Text.ToLower()));
+                else if (type == "Address") Result = SearchClubs.Where(x => x.Address.ToLower().Contains(SearchParam.Text.ToLower()));
+                else Result = SearchClubs.Where(x => x.WorkTime.ToLower().Contains(SearchParam.Text.ToLower()));
+
+                if (Result.Count() > 0)
+                {
+                    Parent.Children.Clear();
+                    foreach (var Club in Result)
+                        Parent.Children.Add(new Elements.Item(Club, this));
+                }
+                else MessageBox.Show("Совпадений не найдено", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else MessageBox.Show("Поле запроса было пустым. Введите текст для сортировки", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
     }
 }
--- Pages/Users/Main.xaml.cs
+++ Pages/Users/Main.xaml.cs
@@ -21,6 +21,7 @@ namespace praktika26.Pages.Users
     /// </summary>
     public partial class Main : Page
     {
+        public ClubContext AllClub = new ClubContext();
         public UserContext AllUsers = new UserContext();
 
         public Main()
@@ -29,11 +30,72 @@ namespace praktika26.Pages.Users
 
             foreach (var user in AllUsers.Users)
                 Parent.Children.Add(new Elements.Item(user, this));
+
+            SearchClub.Items.Add("Выберите клуб ...");
+            SearchClub.SelectedIndex = 0;
+            foreach (var Club in AllClub.Clubs)
+                SearchClub.Items.Add($"{Club.Name}");
         }
 
         private void AddUser(object sender, RoutedEventArgs e)
         {
             MainWindow.init.OpenPages(new Pages.Users.Add(this));
+        }
+
+        private void SortFIO(object sender, RoutedEventArgs e) =>
+            Search("fio");
+
+        private void SortRentStart(object sender, RoutedEventArgs e) =>
+            Search("rent");
+
+        private void SortTime(object sender, RoutedEventArgs e) =>
+            Search("time");
+
+        private void SelectClub(object sender, RoutedEventArgs e)
+        
+        {
+            //TODO: Настроить сортировку
+            if (SearchClub.SelectedIndex >= 0)
+            {
+                var selectedClub = AllClub.Clubs.FirstOrDefault(y => y.Name == SearchClub.Text);
+
+                if (selectedClub != null)
+                {
+                    var results = AllUsers.Users.Where(u => u.IdClub == selectedClub.Id - 1);
+
+                    if (results.Any())
+                    {
+                        Parent.Children.Clear();
+                        foreach (var user in results)
+                            Parent.Children.Add(new Elements.Item(user, this));
+                    }
+                    else
+                    {
+                        MessageBox.Show("Совпадений не найдено", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+            }
+        }
+
+        public void Search(string type)
+        {
+            if (SearchParam.Text != "")
+            {
+                IEnumerable<Models.Users> SearchUsers = AllUsers.Users;
+                IEnumerable<Models.Users> Result = SearchUsers;
+                if (type == "fio") Result = SearchUsers.Where(x => x.FIO.ToLower().Contains(SearchParam.Text.ToLower()));
+                else if (type == "rent") Result = SearchUsers.Where(x => x.RentStart.ToString().Contains(SearchParam.Text));
+                else Result = SearchUsers.Where(x => x.Duration.ToString().Contains(SearchParam.Text));
+
+                if (Result.Count() > 0)
+                {
+                    Parent.Children.Clear();
+                    foreach (var User in Result)
+                        Parent.Children.Add(new Elements.Item(User, this));
+                }
+                else MessageBox.Show("Совпадений не найдено", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else MessageBox.Show("Поле запроса было пустым. Введите текст для сортировки", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
         }
     }
 }
